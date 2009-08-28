7e772c91cd8065ad9f81d4bb0c671003ff79dbdc
Wladimir Palant <trev@adblockplus.org>
2009-08-28 15:28:40 +0200

    Generalized release script and moved it out of extension directories as well.

--- make_release.pl
+++ make_release.pl
@@ -1,32 +1,10 @@
 #!/usr/bin/perl
 
-#############################################################################
-# This is the release automation script, it will change current extension   #
-# version, create release builds and commit it all into Mercurial. Usually  #
-# you just want to create a build - use make_devbuild.pl for this.          #
-#############################################################################
+# This is a dummy, its purpose is to call a script with the same name in the parent directory
 
-use strict;
+our $BRANCH_NAME = "JAVASCRIPT_DEOBFUSCATOR";
 
-die "Version number not specified" unless @ARGV;
-
-my $version = $ARGV[0];
-$version =~ s/[^\w\.]//gs;
-
-open(VERSION, ">version");
-print VERSION $ARGV[0];
-close(VERSION);
-
-@ARGV = ("../downloads/jsdeobfuscator-$version.xpi");
-do './create_xpi.pl';
-
-chdir('..');
-system("hg add downloads/jsdeobfuscator-$version.xpi");
-system(qq(hg commit -m "Releasing JavaScript Deobfuscator $version" downloads jsdeobfuscator));
-
-my $branch = $version;
-$branch =~ s/\./_/g;
-$branch = "JAVASCRIPT_DEOBFUSCATOR_".$branch."_RELEASE";
-system(qq(hg tag $branch));
-
-system(qq(hg push));
+$0 =~ s/(.*[\\\/])//g;
+chdir($1) if $1;
+do "../$0";
+die $@ if $@;
