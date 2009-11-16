b9a8e25f7ca8b1376386af033d4b47f7b0e134b7
Wladimir Palant <trev@adblockplus.org>
2009-11-16 16:06:00 +0100

    Made build scripts check out buildtools repository if it isn't there

--- make_release.pl
+++ make_release.pl
@@ -6,5 +6,8 @@ our $BRANCH_NAME = "JAVASCRIPT_DEOBFUSCATOR";
 
 $0 =~ s/(.*[\\\/])//g;
 chdir($1) if $1;
+
+system("hg", "clone", "https://hg.adblockplus.org/buildtools/") unless -e "buildtools";
+
 do "buildtools/$0";
 die $@ if $@;
